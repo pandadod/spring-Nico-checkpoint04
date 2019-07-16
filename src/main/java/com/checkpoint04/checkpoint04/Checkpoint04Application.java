dc8ada3e027df25a4f61064c7398285acc89d110
nico <nicolas.raoux@gmail.com>
2019-07-16 11:44:02 +0200

    creation depot github et remplissage application.properties

---
+++ src/main/java/com/checkpoint04/checkpoint04/Checkpoint04Application.java
@@ -0,0 +1,13 @@
+package com.checkpoint04.checkpoint04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Checkpoint04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Checkpoint04Application.class, args);
+	}
+
+}
